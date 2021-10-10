6cda2555a519e381700ed46a96a7c2dc0a61beab
Efraín Báez <e.german@hotmail.com>
2021-10-09 19:11:41 -0500

    Add decoding

--- app.py
+++ app.py
@@ -23,3 +23,31 @@ for v, k in string_index.items():
     encoded_text += k[1]
 
 print(encoded_text)
+
+# Decode
+i = 0
+index_string = defaultdict(int)
+index = 1
+prefix_str = ""
+while i < len(encoded_text):
+
+    while encoded_text[i].isdigit():
+        prefix_str += encoded_text[i]
+        i += 1
+
+    prefix = int(prefix_str)
+    if prefix not in index_string:
+        index_string[index] = encoded_text[i]
+    else:
+        index_string[index] = index_string[prefix] + encoded_text[i]
+
+    i += 1
+    index += 1
+    prefix_str = ""
+
+
+decoded_text = ""
+for v, k in index_string.items():
+    decoded_text += k
+
+print(decoded_text)
